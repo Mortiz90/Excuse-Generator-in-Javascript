81151180b99cf53ececbbe0027bc26ae878a07dc
Mauricio Reinales <146978569+Mortiz90@users.noreply.github.com>
2023-10-14 18:17:20 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
+};
+
+let excuseGenerator = () => {
+  let who = ["My dog", "My grandma", "My Wife", "My computer"];
+  let action = ["ate", "unplugged", "destroyed", "broke"];
+  let what = ["my homework", "the keys", "the house"];
+  let when = [
+    "before the exam",
+    "just in time",
+    "hen I finished",
+    "hen i was sleeping",
+    "hen I was doing the homework"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return `${who[whoIndx]} ${action[actionIndx]} ${what[whatIndx]} ${when[whenIndx]}`;
 };
